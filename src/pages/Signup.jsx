adeeb37cac88b8ca3e84560329d0406d14529245
haon <haoony@naver.com>
2025-02-11 15:01:30 +0900

    ㄹ

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,21 @@
 export default function Signup(){
+    // 팀버랜드 참고해서 만들기
     return (
         <>
-        <div className="content-wrap">
-            <h1>Signup</h1> 
+        <div className="signupBox">
+            <section className="signup-wrap">
+                <h1>회원가입</h1> 
+                <ul>
+                    <li>
+                        <label htmlFor=""></label>
+                        <input type="text" />
+                    </li>
+                </ul>
+            </section>
         </div>
         </>
     );
 }
+
+
+
